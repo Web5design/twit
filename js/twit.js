57c61bbb8549b0ded87bffd433186f381e41a477
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-10-30 12:23:45 -0400

    Fixed API URL call and now includes retweets and option to exclude replies

--- js/twit.js
+++ js/twit.js
@@ -82,10 +82,12 @@ function relative_time(time_value) {
 window.onload = function() {
     var ajax_load            = "<img class='twit-loader' src='img/loading.gif' alt='Loading...'>",
         twitter_preferences  = {
-            count    : 2,
-            username : 'gryghostvisuals'
+            count    : 1,
+            username : 'gryghostvisuals',
+            retweets : true,
+            replies : false
         },
-        twitterUrl = 'http://twitter.com/statuses/user_timeline.json?screen_name=' + twitter_preferences.username + '&callback=twitterCallback&count=' + twitter_preferences.count,
+        twitterUrl = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=' + twitter_preferences.username + '&callback=twitterCallback&count=' + twitter_preferences.count + '&include_rts=' + twitter_preferences.retweets + '&exclude_replies=' + twitter_preferences.replies;
         script     = document.createElement('script');
 
     $("#twitter_feed").html(ajax_load);
